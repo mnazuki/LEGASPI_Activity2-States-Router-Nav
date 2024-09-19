b4f5df67caab43dac44281a2119bfcf990679648
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2024-09-19 15:57:50 +0800

    Changed to React Bootstrap

--- src/App.js
+++ src/App.js
@@ -7,30 +7,29 @@ import About from './components/About.js';
 import Contact from './components/Contact.js';
 import NotFound from './components/NotFound.js';
 import {NavLink} from "react-router-dom"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className = "App">
       <Router>
-        <div>
-          <nav>        
-            <ul>
-              <li>
-                <NavLink to="/home">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/counter">Counter</NavLink></li>
-              <li>
-                <NavLink to="/about">About</NavLink>
-              </li>
-              <li>
-                <NavLink to="/contact">Contact</NavLink>
-              </li>
-            </ul>
-          </nav> 
-        </div>
-        
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand as={Link}to="/home">Activity 2</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link as={Link}to="/home">Home</Nav.Link>
+            <Nav.Link as={Link}to="/counter">Counter</Nav.Link>
+            <Nav.Link as={Link}to="/about">About</Nav.Link>
+            <Nav.Link as={Link}to="/contact">Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>     
         <Routes>
           <Route index element={<Dashboard/>}/>
           <Route path="/home" element={<Home/>}/>
